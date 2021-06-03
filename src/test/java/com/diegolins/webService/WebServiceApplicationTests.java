63e7a2f3b8c1453caba64787e176f18e2f90627f
Diego Lins <68483773+DiegoLins10@users.noreply.github.com>
2021-06-03 18:59:25 -0300

    inicial project

---
+++ src/test/java/com/diegolins/webService/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegolins.webService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
